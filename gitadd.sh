94b588ace6244e43a8516ad5e9aad93aeb1473c5
Andrey A. Moiseenko <ip@maasoft.ru>
2026-05-22 15:05:22 +0300

    ~

--- gitadd.sh
+++ gitadd.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 git add *.h *.cpp *.nasm des/*.h des/*.c des/*.cpp sha256/*.h sha256/*.cpp
-git add gitadd.sh
+git add gitadd.sh .github
 git add CryptLib.vcxproj CryptLib.vcxproj.filters Makefile Licence.txt
 
 if [ x"$1" = "x" ]; then
     #echo 'git commit -m "Comment"'
     #echo 'git push origin master'
-    git commit -m '~' && git push origin main
+    git commit -m '~' ; git push origin main
 else
-    git commit -m "$1" && git push origin main
+    git commit -m "$1" ; git push origin main
 fi
